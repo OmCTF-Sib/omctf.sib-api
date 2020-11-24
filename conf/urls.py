f3d716770129c9e92474a41c95a03795c93e3230
Dmitry Bibik <bibikdima@gmail.com>
2020-11-25 00:39:14 +0600

    fix admin url

--- conf/urls.py
+++ conf/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path(settings.ADMIN_URL, admin.site.urls),
+    path("admin/", admin.site.urls),
 
     url(r"^api/v1/", include("apps.news.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
