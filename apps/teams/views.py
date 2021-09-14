84c4df25cd788641d9f7363d54c3a6568373a972
Bibik Dima <bibikdima@gmail.com>
2021-09-14 12:19:46 +0300

    feat: add filter for team_type

--- apps/teams/views.py
+++ apps/teams/views.py
@@ -5,11 +5,14 @@ from apps.teams.models import Team
 from apps.teams.permissions import TeamAccessPolicy
 from apps.teams.serializers import TeamSerializer
 from apps.utils.mixins import MultiSerializerViewSetMixin
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class TeamModelViewSet(MultiSerializerViewSetMixin, ModelViewSet):
     permission_classes = (TeamAccessPolicy,)
     serializer_class = TeamSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('team_type',)
 
     def get_queryset(self) -> Union[List, QuerySet]:
         return Team.objects.filter(is_visible=True).prefetch_related('participants').all()
